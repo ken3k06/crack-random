46499f393dc1a5f1ddea2200a4b9761d7e17f64c
Le Tri Duc <159978981+ken3k06@users.noreply.github.com>
2025-09-15 21:47:27 +0700

    Update implement.py

--- Go/implement.py
+++ Go/implement.py
@@ -1,71 +1,59 @@
 from rng_cooked import rng_cooked
-
-RNG_LEN = 607
-RNG_TAP = 273
-RNG_MAX = 1 << 63
-RNG_MASK = RNG_MAX - 1
-INT32_MAX = (1 << 31) - 1
-
+# const: 
+rngLen = 607
+rngTap = 273
+rngMax = 1 << 63
+rngMask = rngMax - 1
+int32max = (1 << 31) - 1
 
 def seedrand(x):
-    """Seed function for rng: x[n+1] = 48271 * x[n] mod (2**31 - 1)"""
     A = 48271
     Q = 44488
     R = 3399
-
-    hi = x // Q
+    hi = x  // Q
     lo = x % Q
-    x = A * lo - R * hi
+    x = A*lo - R*hi
     if x < 0:
-        x += INT32_MAX
+        x += int32max
     return x
-
-
-class RngSource:
-    def __init__(self, mt = None):
-        self.tap = 0
-        self.feed = RNG_LEN - RNG_TAP
-        if mt is not None:
-            self.vec = list(mt)
-        else:
-            self.vec = [0] * RNG_LEN
-
-    def seed(self, seed):
-        self.tap = 0
-        self.feed = RNG_LEN - RNG_TAP
-
-        seed %= INT32_MAX
+class RNGSource:
+    def __init__(self):
+        self.tap = 0 
+        self.feed = rngLen - rngTap
+        self.vec = [0] * rngLen
+    def seed(self,seed):
+        self.tap = 0 
+        self.feed = rngLen - rngTap
+        
+        seed = seed % int32max
         if seed < 0:
-            seed += INT32_MAX
+            seed += int32max
         if seed == 0:
             seed = 89482311
-
         x = int(seed)
-        for i in range(-20, RNG_LEN):
+        for i in range(-20,rngLen):
             x = seedrand(x)
-            if i >= 0:
+            if i >=0 :
                 u = (int(x) << 40) & 0xFFFFFFFFFFFFFFFF
                 x = seedrand(x)
                 u ^= (int(x) << 20) & 0xFFFFFFFFFFFFFFFF
                 x = seedrand(x)
-                u ^= int(x)
+                u ^= (int(x) & 0xFFFFFFFFFFFFFFFF)
                 u ^= rng_cooked[i]
                 self.vec[i] = u
-
     def int63(self):
-        """Returns a non-negative pseudo-random 63-bit integer"""
-        return self.uint64() & RNG_MASK
-
+        return self.uint64() & rngMask
     def uint64(self):
-        """Returns a non-negative pseudo-random 64-bit integer"""
         self.tap -= 1
         if self.tap < 0:
-            self.tap += RNG_LEN
-
+            self.tap += rngLen
         self.feed -= 1
         if self.feed < 0:
-            self.feed += RNG_LEN
-
-        x = (self.vec[self.feed] + self.vec[self.tap]) & 0xFFFFFFFFFFFFFFFF
+            self.feed += rngLen
+        x = self.vec[self.feed] + self.vec[self.tap]
         self.vec[self.feed] = x
-        return x
+        return int(x) & 0xFFFFFFFFFFFFFFFF
+rng = RNGSource()
+rng.seed(1234)
+output = [rng.uint64() for _ in range(123)]
+print(output)
